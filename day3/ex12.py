4b04e6430c3b0adeb6b765a1839507d359a5485f
dsky1990 <dutian1990@gmail.com>
2018-10-21 17:26:28 +0800

    fix ex12

---
+++ day3/ex12.py
@@ -0,0 +1,5 @@
+age = int(input("How old are you? "))
+height = int(input("How tall are you? "))
+weight = int(input("How weight are you? "))
+
+print("So, you're %d old, %d tall and %d heavy" %(age, height,weight))
